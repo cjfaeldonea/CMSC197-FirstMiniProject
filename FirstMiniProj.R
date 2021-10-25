280b567a7d20b30ddf205689ba1f9333e16b9c3e
Candyl John Faeldonea <cffaeldonea@up.edu.ph>
2021-10-25 15:51:49 +0800

    Added #4 and comments

--- FirstMiniProj.R
+++ FirstMiniProj.R
@@ -20,6 +20,9 @@ pollutantmean <- function(directory, pollutant, id=1:322)
   mean(collection, na.rm=TRUE)
 }
 
+
+
+
 #complete
 complete <- function(directory, id =1:332)
 {
@@ -45,28 +48,62 @@ complete <- function(directory, id =1:332)
 }
 
 
+
+
 #correlation 
 corr <- function(directory, threshold =0)
 {
-  #loading all files into a list of files called "FileNames"
-  fileNames <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
+  #loading all files into a list of files called "FileNames" with formulat specdata\xxx.csv
+  fileNames <- list.files(path = directory, pattern = ".csv", full.name = TRUE)
   
-  correlation = numeric()
+  #variable for completes 
+  df <- complete(directory)
   
-  for (i in 1:322) {
-    #data of each csv
-    data = read.csv(fileNames[i])
+  #calculate and store the IDs of nobs that are over the threshold
+  ids <- df[df["nobs"] > threshold, ]$id
+  
+  #initialized empty correlation as numeric
+  corrr <- numeric()
+  
+  
+  #for each CSV files in ID
+  for (i in ids){
+    #read the csv
+    mydata <- read.csv(fileNames[i])
     
-    #if threshold is reached
-    if (sum(complete.cases(data))>threshold) 
-    {
-      correlation <- c(correlation, cor(data[["sulfate"]], data[["nitrate"]],use = "complete.obs"))
-    }
+    #calculate and store the number of complete cases 
+    dff <- mydata[complete.cases(mydata),]
     
+    #calculate the complete cases that reached the threshold
+    corrr <- c(corrr, cor(dff$sulfate, dff$nitrate))
   }
-  
-  correlation
+  return(corrr)
 }
+
+
+#Histogram
+# Ploting the 30-day mortality rates for heart attack
+
+# Reading in data from CSV and specify that we need to first read the data as character
+outcome <- read.csv('outcome-of-care-measures.csv', colClasses = "character")
+
+#we convert the data to numeric
+outcome[, 11] <- as.numeric(outcome[, 11])
+
+#this outputs the historgram with a title, x-label, and with color.
+hist(outcome[, 11],
+     xlab = paste("Deaths"),
+     main=paste("Hospital 30-Day Death (Mortality) Rates from Heart Attack"),
+     col="lightblue")
+
+
+
+
+
+
+
+#example test cases
+
 
 
 
@@ -75,5 +112,16 @@ pollutantmean("specdata","sulfate",1:10)
 pollutantmean("specdata","nitrate",70:72)
 pollutantmean("specdata","nitrate",23)
 
+complete("specdata",1)
 complete("specdata",c(2,4,8,10,12))
+complete("specdata",30:25)
+complete("specdata",3)
 
+cr <- corr("specdata", 150)
+head(cr); summary(cr)
+
+cr <- corr("specdata", 400)
+head(cr); summary(cr)
+
+cr <- corr("specdata", 5000)
+head(cr); summary(cr)
